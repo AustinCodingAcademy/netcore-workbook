968ba0c1b1c18f524243e489084806cb5eb74825
Dylan Zocchi <dxocchi@gmail.com>
2018-08-02 18:59:12 -0500

    popquiz

---
+++ PopQuiz/PopQuiz/Controllers/PopQuizController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+namespace PopQuiz.Controllers
+{
+    public class PopQuizController : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ PopQuiz/PopQuiz/Models/PopModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace PopQuiz.Models
+{
+    public class PopModel
+    {
+        public PopModel()
+        {
+        }
+    }
+}
