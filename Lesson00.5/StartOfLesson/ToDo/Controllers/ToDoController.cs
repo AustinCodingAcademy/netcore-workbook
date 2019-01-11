07ae1fe36c6b22c37cfd7135de51b2d5d7f68d1b
James.Woodard <james@jungledisk.com>
2019-01-11 16:28:48 -0600

    uploading project so far

---
+++ Lesson00.5/StartOfLesson/ToDo/Controllers/ToDoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ToDoApp.Models;
+
+namespace ToDoApp.Controllers
+{
+    public class ToDoController : Controller
+    {
+
+        public List<Models.ToDo> listOfTodo = new List<Models.ToDo>()
+        {
+            new Models.ToDo{ Id = 1, Description = "Do a thing", Status = new Status() { Id = 1, Value = "Done" } },
+            new Models.ToDo{ Id = 2, Description = "Do another thing", Status = new Status() { Id = 2, Value = "Pending" }  },
+            new Models.ToDo{ Id = 3, Description = "Don't do nothin'", Status = new Status() { Id = 3, Value = "Not Done" }  },
+        };
+        public IActionResult Index()
+        {
+            return View(listOfTodo);
+        }
+    }
+}
