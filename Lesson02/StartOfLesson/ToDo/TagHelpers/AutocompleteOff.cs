b02b073161266426d3ca13baeea061e4b672e7bd
James.Woodard <james@jungledisk.com>
2019-01-21 11:09:26 -0600

    added social medial links as view components on home page

---
+++ Lesson02/StartOfLesson/ToDo/TagHelpers/AutocompleteOff.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+namespace ToDoApp.TagHelpers
+{
+    [HtmlTargetElement("*", Attributes = "autocomplete-off")]
+    public class AutocompleteOffTagHelper : TagHelper
+    {
+
+        
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+
+            output.Attributes.SetAttribute("autocomplete", "off");
+
+        }
+    }
+}
